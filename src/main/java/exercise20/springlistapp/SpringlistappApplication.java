5736b07b95315d69b4914f7f1e43622f60ba02b8
Pavlo <pawel.wowniuk@gmail.com>
2019-03-18 12:56:46 +0100

    Zadanie20 Dosyłam plik Pom.xml Dodałem klasę startową

---
+++ src/main/java/exercise20/springlistapp/SpringlistappApplication.java
@@ -0,0 +1,13 @@
+package exercise20.springlistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlistappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlistappApplication.class, args);
+    }
+
+}
